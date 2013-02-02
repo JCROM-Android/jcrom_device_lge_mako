09933220ba771c06e1a8d7166248c1409d6b2298
Akihiro Maeda <sola.1980.a@gmail.com>
2013-02-02 23:37:33 +0900

    update android-4.2.1_r1.2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -90,6 +90,8 @@ TARGET_NO_RPC := true
 
 TARGET_RELEASETOOLS_EXTENSIONS := device/lge/mako
 
+BOARD_CHARGER_ENABLE_SUSPEND := true
+
 DISABLE_DEXPREOPT := true
 
 -include vendor/lge/mako/BoardConfigVendor.mk
