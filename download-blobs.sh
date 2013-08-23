7f3763e084fd8808a8f06606e92898b3d14df007
Akihiro Maeda <sola.1980.a@gmail.com>
2013-08-24 01:39:42 +0900

    Updated the "Binaries for Nexus Devices and Factory Image" to JWR66Y.

--- download-blobs.sh
+++ download-blobs.sh
@@ -34,9 +34,9 @@ install_blobs() {
     done
 }
 
-MAGURO_BLOBS="broadcom-mako-jwr66v-cbde0d61.tgz
-              lge-mako-jwr66v-985845e4.tgz
-              qcom-mako-jwr66v-30ef957c.tgz"
+MAGURO_BLOBS="broadcom-mako-jwr66y-137ef66d.tgz
+              lge-mako-jwr66y-a85ca75e.tgz
+              qcom-mako-jwr66y-a5becaf1.tgz"
 
 CSUM_LIST="$PWD/blob-shasums"
 
