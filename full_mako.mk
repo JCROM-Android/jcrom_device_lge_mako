8f08933e6ef907e87e006b37ac0fc9e8bb9a38b7
Akihiro Maeda <sola.1980.a@gmail.com>
2013-12-13 22:47:53 +0900

    Updated the JCROM to android 4.4.2_r1.

--- full_mako.mk
+++ full_mako.mk
@@ -33,7 +33,7 @@ PRODUCT_PACKAGES += \
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=occam TARGET_DEVICE=mako BUILD_FINGERPRINT=google/occam/mako:4.4/KRT16O/907817:user/release-keys PRIVATE_BUILD_DESC="occam-user 4.4 KRT16O 907817 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=occam TARGET_DEVICE=mako BUILD_FINGERPRINT=google/occam/mako:4.4.2/KOT49H/937116:user/release-keys PRIVATE_BUILD_DESC="occam-user 4.4.2 KOT49H 937116 release-keys"
 
 PRODUCT_NAME := full_mako
 PRODUCT_DEVICE := mako
