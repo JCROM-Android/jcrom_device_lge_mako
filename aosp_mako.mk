d0a823013f801b7dfa619740b126064113129e17
Akihiro Maeda <sola.1980.a@gmail.com>
2014-11-26 20:08:47 +0900

    Add the definition of AOSP build

--- aosp_mako.mk
+++ aosp_mako.mk
@@ -13,6 +13,34 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-$(call inherit-product, device/lge/mako/full_mako.mk)
+
+# Sample: This is where we'd set a backup provider if we had one
+# $(call inherit-product, device/sample/products/backup_overlay.mk)
+
+# Live Wallpapers
+PRODUCT_PACKAGES += \
+        LiveWallpapers \
+        LiveWallpapersPicker \
+        VisualizationWallpapers
+
+
+# Get the long list of APNs
+PRODUCT_COPY_FILES := device/lge/mako/apns-full-conf.xml:system/etc/apns-conf.xml
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 PRODUCT_NAME := aosp_mako
+PRODUCT_DEVICE := mako
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := AOSP on Mako
+PRODUCT_MANUFACTURER := LGE
+PRODUCT_RESTRICT_VENDOR_FILES := true
+
+# Inherit from hardware-specific part of the product configuration
+$(call inherit-product, device/lge/mako/device.mk)
+$(call inherit-product-if-exists, vendor/lge/mako/device-vendor.mk)
+
+#AOSP
+$(call inherit-product-if-exists, vendor/aosp/lge/mako/aosp.mk)
+$(call inherit-product, device/aosp/common/aosp.mk)
